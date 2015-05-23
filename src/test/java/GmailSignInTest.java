28575f03f1bdd4722ba257a9be2ad3cee4168238
Ken Ho <appsenseca@gmail.com>
2015-05-23 18:53:26 -0400

    [Cross-Browser] added support for Chrome, precondition is you need to setup chromedriver and install Chrome browser in your local environment

--- src/test/java/GmailSignInTest.java
+++ src/test/java/GmailSignInTest.java
@@ -6,13 +6,26 @@ import com.appsenseca.pageobjects.SignInPage;
 import com.appsenseca.util.WebUtil;
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class GmailSignInTest {
-    WebDriver driver = new FirefoxDriver();
+
+    WebDriver driver;
+
+    @Before
+    public void setDriver(){
+        String browserName = System.getenv("browser");
+        if(browserName!= null && browserName.equalsIgnoreCase("Chrome")){
+            driver = new ChromeDriver();
+        }else{
+            driver = new FirefoxDriver();
+        }
+    }
 
     @Category({Critical.class})
     @Test
